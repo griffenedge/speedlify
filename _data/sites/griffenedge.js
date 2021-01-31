526855e489dd3be1ca3547d511f2f11283e2d84a
Griffen Edge <11290702+griffenedge@users.noreply.github.com>
2021-01-31 23:54:07 +0000

    sites

---
+++ _data/sites/griffenedge.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "griffenedge.com", // optional, falls back to object key
+	description: "Griffen's website",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.griffenedge.com/",
+		"https://www.griffenedge.com/about/",
+		"https://www.griffenedge.com/adlight/",
+		"https://www.griffenedge.com/prioritab/",
+		"https://www.griffenedge.com/astronome/"
+	]
+};
---
+++ _data/sites/sydney-community-college.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: "sydneycommunitycollege.edu.au", // optional, falls back to object key
+	description: "Sydney Community College website",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		// 10 most popular pages
+		"https://www.sydneycommunitycollege.edu.au/",
+		"https://www.sydneycommunitycollege.edu.au/courses/arts",
+		"https://www.sydneycommunitycollege.edu.au/courses",
+		"https://www.sydneycommunitycollege.edu.au/checkout",
+		"https://www.sydneycommunitycollege.edu.au/courses/lifestyle",
+		"https://www.sydneycommunitycollege.edu.au/course/srfi37",
+		"https://www.sydneycommunitycollege.edu.au/course/wwbg",
+		"https://www.sydneycommunitycollege.edu.au/courses/learn-language",
+		"https://www.sydneycommunitycollege.edu.au/course/cmws",
+		"https://www.sydneycommunitycollege.edu.au/courses/arts/painting"
+	]
+};
